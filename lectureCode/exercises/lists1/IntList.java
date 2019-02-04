e886138104144d86cbbe3f1b4ef9f0fb79cc27f1
Kevin Yu <kevin.yu@rutgers.edu>
2019-02-04 16:16:05 -0500

    Add lecture code

--- lectureCode/exercises/lists1/IntList.java
+++ lectureCode/exercises/lists1/IntList.java
@@ -9,29 +9,17 @@ public class IntList {
 
 	/** Return the size of the list using... recursion! */
 	public int size() {
-		if (rest == null) {
-			return 1;
-		}
-		return 1 + this.rest.size();
+		return 0;
 	}
 
 	/** Return the size of the list using no recursion! */
 	public int iterativeSize() {
-		IntList p = this;
-		int totalSize = 0;
-		while (p != null) {
-			totalSize += 1;
-			p = p.rest;
-		}
-		return totalSize;
+		return 0;
 	}
 
-	/** Returns the ith item of this IntList. */
+	/** Returns the ith value in this list.*/
 	public int get(int i) {
-		if (i == 0) {
-			return first;
-		}
-		return rest.get(i - 1);
+		return 0;
 	}
 
 	public static void main(String[] args) {
@@ -39,6 +27,6 @@ public class IntList {
 		L = new IntList(10, L);
 		L = new IntList(5, L);
 
-		System.out.println(L.get(1));
+		System.out.println(L.iterativeSize());
 	}
 } 
