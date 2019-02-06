ace7e91cf461da42278e58e8ea1cf5c051d4086b
Kevin Yu <kevin.yu@rutgers.edu>
2019-02-06 01:48:00 -0500

    Add function calcNetForceExertedByX

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 /**
  * Planet
  */
@@ -43,13 +45,53 @@ public class Planet {
 
     /**
      * Usage:samh.calcForceExertedBy(rocinante);
-     * Funtion: Return a double describing the force exerted on this planet by the given planet.
+     * Funtion: Return a double describing the force exerted on this planet 
+     *          by the given planet.
      */
     public double calcForceExertedBy(Planet p2) {
         Planet p1 = this;
-        double force;
+        double force = 0;
         force = G * (p1.mass) * (p2.mass) / Math.pow((p1.calcDistance(p2)),2);
         return force;
+    }
+
+    /**
+     * Usage:samh.calcForceExertedByX(rocinante);
+     * Funtion:  Return the total force, these two methods describe the force 
+     * exerted in the X and Y directions.
+     */
+    public double calcForceExertedByX(Planet p2) {
+        Planet p1 = this;
+        double forceX = 0;
+        forceX = -p1.calcForceExertedBy(p2) * (p1.xxPos - p2.xxPos) / p1.calcDistance(p2);
+        return forceX;
+    }
+
+    public double calcForceExertedByY(Planet p2) {
+        Planet p1 = this;
+        double forceY = 0;
+        forceY = -p1.calcForceExertedBy(p2) * (p1.yyPos - p2.yyPos) / p1.calcDistance(p2);
+        return forceY;
+    }
+
+    /**
+     * Usage:samh.calcNetForceExertedByX(allPlanets);
+     * Funtion: Take in an array of Planets and calculate the net X force 
+     *          exerted by all planets in that array upon the current Planet.
+     */
+    public double calcNetForceExertedByX(Planet[] Planets) {
+        Planet p1 = this;
+        double nforceX = 0;
+        for (int i = 0; i < Planets.length; i++) {
+            if (!p1.equals(Planets[i])){
+                nforceX = nforceX + p1.calcForceExertedByX(Planets[i]);
+
+                //force = G * (p1.mass) * (Planets[i].mass) / Math.pow((p1.calcDistance(Planets[i])), 2);
+                //dX = p1.xxPos - Planets[i].xxPos;
+                //forceX = forceX + force * dX / p1.calcDistance(Planet[i]);
+            }
+        }
+        return nforceX;
     }
 
 
